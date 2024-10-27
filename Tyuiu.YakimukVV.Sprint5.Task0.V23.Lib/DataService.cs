c30eb20d9a607b2cf532810dc8db962180e024f7
Владислав Якимук <Tyuiu.YakimukVV@outlook.com>
2024-10-27 19:31:46 +0500

    Task0

--- Tyuiu.YakimukVV.Sprint5.Task0.V23.Lib/DataService.cs
+++ Tyuiu.YakimukVV.Sprint5.Task0.V23.Lib/DataService.cs
@@ -8,8 +8,8 @@ namespace Tyuiu.YakimukVV.Sprint5.Task0.V23.Lib
     {
         public string SaveToFileTextData(int x)
         {
-            double y = (1 + x * x * x) / (x * x);
-            y = Math.Round(y, 2);
+            double y = (1 + Math.Pow(x, 3)) / Math.Pow(x, 2);
+            y = Math.Round(y, 3);
 
             string filePath = Path.Combine(Path.GetTempPath(), "OutPutFileTask0.txt");
 
