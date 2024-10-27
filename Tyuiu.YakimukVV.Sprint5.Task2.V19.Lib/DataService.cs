7ee5d77dafdc0a6f45cce3c8bc05ba11f1e2f7f5
Владислав Якимук <Tyuiu.YakimukVV@outlook.com>
2024-10-27 20:43:26 +0500

    Task2

--- Tyuiu.YakimukVV.Sprint5.Task2.V19.Lib/DataService.cs
+++ Tyuiu.YakimukVV.Sprint5.Task2.V19.Lib/DataService.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.IO;
-using System.Text;
 using tyuiu.cources.programming.interfaces.Sprint5;
 
 namespace Tyuiu.YakimukVV.Sprint5.Task2.V19.Lib
@@ -9,21 +8,35 @@ namespace Tyuiu.YakimukVV.Sprint5.Task2.V19.Lib
     {
         public string SaveToFileTextData(int[,] matrix)
         {
-            string filePath = Path.Combine(Path.GetTempPath(), "OutPutFileTask2.csv");
-
-            StringBuilder csvContent = new StringBuilder();
             for (int i = 0; i < matrix.GetLength(0); i++)
             {
                 for (int j = 0; j < matrix.GetLength(1); j++)
                 {
-                    csvContent.Append(matrix[i, j]);
-                    if (j < matrix.GetLength(1) - 1)
-                        csvContent.Append(";");
+                    if (matrix[i, j] % 2 != 0)
+                    {
+                        matrix[i, j] = 0;
+                    }
                 }
-                csvContent.AppendLine();
             }
 
-            File.WriteAllText(filePath, csvContent.ToString());
+            string filePath = Path.Combine(Path.GetTempPath(), "OutPutFileTask2.csv");
+
+            using (StreamWriter writer = new StreamWriter(filePath))
+            {
+                for (int i = 0; i < matrix.GetLength(0); i++)
+                {
+                    for (int j = 0; j < matrix.GetLength(1); j++)
+                    {
+                        writer.Write(matrix[i, j]);
+                        if (j < matrix.GetLength(1) - 1)
+                        {
+                            writer.Write(";");
+                        }
+                    }
+                    writer.WriteLine();
+                }
+            }
+
             return filePath;
         }
     }
