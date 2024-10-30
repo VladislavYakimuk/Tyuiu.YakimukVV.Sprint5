7ad0aa86858ab56117d37144e920e7dc18dd798c
Владислав Якимук <Tyuiu.YakimukVV@outlook.com>
2024-10-30 19:10:45 +0500

    Task7

--- Tyuiu.YakimukVV.Sprint5.Task7.V24.Lib/DataService.cs
+++ Tyuiu.YakimukVV.Sprint5.Task7.V24.Lib/DataService.cs
@@ -12,7 +12,10 @@ namespace Tyuiu.YakimukVV.Sprint5.Task7.V24.Lib
 
             string modifiedData = Regex.Replace(data, @"\b[а-яА-ЯёЁ]+\b", "слово");
 
-            string outputPath = Path.Combine(Path.GetDirectoryName(path), "OutPutDataFileTask7V24.txt");
+            string directoryPath = Path.GetDirectoryName(path) ?? Path.GetTempPath();
+            string outputPath = Path.Combine(directoryPath, "OutPutDataFileTask7V24.txt");
+
+            Directory.CreateDirectory(directoryPath);
 
             File.WriteAllText(outputPath, modifiedData);
 
